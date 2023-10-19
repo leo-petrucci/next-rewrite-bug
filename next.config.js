69fbd499d604cd3fa694d9a1b5480dfa174bf4ac
Leonardo Petrucci <leo.petrucci@rvu.co.uk>
2023-10-19 11:57:12 +0100

    Broken config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  rewrites: async () => [
+    {
+      source: `/_next/data/:release/more-specific/:path*`,
+      destination: `https://example.com`,
+    },
+    {
+      source: `/test-app/subfolder/:release/more-specific/:path*`,
+      destination: `https://example.com`,
+    },
+  ],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
